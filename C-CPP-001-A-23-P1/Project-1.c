369110644be0ac2e0cb31d60d999cf7e6dafccf8
RDM <70675143+RDMcLeod@users.noreply.github.com>
2023-03-11 21:48:35 -0500

    updates

--- C-CPP-001-A-23-P1/Project-1.c
+++ C-CPP-001-A-23-P1/Project-1.c
@@ -6,8 +6,8 @@ int main()
     int lower, upper, step;
 
     lower = 0;          /*tc lower limit of temperature table */
-    upper = 1000;        /* upper limit */
-    step = 20;          /*tc step size */
+    upper = 300;        /* upper limit */
+    step = 1;          /*tc step size */
 
     fahr = lower;
     while (fahr <= upper) {
